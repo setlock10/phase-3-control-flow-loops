2529bcff9d5a1d431dd0da79b6652003f84974ef
John J Setlock II <johnsetlock@gmail.com>
2022-08-27 13:47:55 -0500

    Completed Assignment

--- looping.rb
+++ looping.rb
@@ -1,5 +1,14 @@
+require 'pry'
+
 def happy_new_year
   # your code here
+  i=10
+  while i > 0
+    puts i
+    i -=1
+  end
+  puts "Happy New Year!"
+
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +26,25 @@ end
 
 def fizzbuzz_printer
   # your code here
+  i=1
+  while i<101
+    puts fizzbuzz(i)
+    i+=1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  i=str.length
+  newstr=""
+  while i>0
+    newstr=newstr+str[i-1]
+    i=i-=1
+  end
+  puts newstr
+  return newstr
+
+
 end
+
+# binding.pry
